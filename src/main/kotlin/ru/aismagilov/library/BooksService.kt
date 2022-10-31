a371a53a867b42d3cc8caa663adf7a33db99b17a
Azat Ismagilov <azatismagilov00@gmail.com>
2022-10-31 04:04:50 +0300

    Correct status handling

--- src/main/kotlin/ru/aismagilov/library/BooksService.kt
+++ src/main/kotlin/ru/aismagilov/library/BooksService.kt
@@ -1,21 +1,23 @@
 package ru.aismagilov.library
 
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class BooksService(private val booksRepository: BooksRepository) {
     fun getAll(): List<Book> = booksRepository.findAll()
 
-    fun getById(id: Long): Book = booksRepository.findById(id).orElseThrow { NotFoundException() }
+    fun getById(id: Long): Book = booksRepository.findById(id)
+        .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND, "There is no book with such id") }
 
     fun create(book: Book): Book = booksRepository.save(book)
 
     fun updateById(id: Long, book: Book): Book =
         if (booksRepository.existsById(id)) booksRepository.save(book.copy())
-        else throw NotFoundException()
+        else throw ResponseStatusException(HttpStatus.NOT_FOUND, "There is no book with such id")
 
     fun deleteById(id: Long) =
         if (booksRepository.existsById(id)) booksRepository.deleteById(id)
-        else throw NotFoundException()
+        else throw ResponseStatusException(HttpStatus.NOT_FOUND, "There is no book with such id")
 }
